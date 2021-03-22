76cab00a3e815068792435bbf71f0aa275d3aff6
mbritov <mikebritoff@gmail.com>
2021-03-22 11:45:56 +0100

    Added TodoController with endpoints to Create/Update and GetAll

---
+++ Todo.Web/Controllers/TodoController.cs
@@ -0,0 +1,32 @@
+﻿namespace Todo.Core.Web.Controller
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+    using Todo.Application;
+
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ITodoService _service;
+
+        public TodoController(ITodoService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        [Route("Todo")]
+        public IEnumerable<TodoDto> GetAll()
+        {
+            var userId = 1; // todo - send as an parameter
+            return _service.GetAll(userId);
+        }
+
+        [HttpPut]
+        [Route("Todo")]
+        public void Put(TodoDto task)
+        {
+            _service.UpdateTask(task);
+        }
+    }
+}
